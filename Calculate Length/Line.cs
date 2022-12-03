e8a408f02e1348bcf49bbe33938edc215bb9a652
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-03 19:29:47 +0530

    Calculate length by using Cartesian system

---
+++ Calculate Length/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculate_Length
+{
+    internal class Line
+    {
+        public static double x1, y1, x2, y2, length_of_line;
+        public static void Coordinates()
+        {
+            length_of_line = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1 , 2));
+            Console.WriteLine("Length of line : " + length_of_line);
+        }
+    }
+}
---
+++ Calculate Length/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculate_Length
+{
+    internal class Program
+    {
+       
+       static void Main(string[] args)
+        {
+             Console.Write("Enter value for x1: ");
+             Line.x1 = Convert.ToInt32(Console.ReadLine());
+             Console.Write("Enter value for x2: ");
+             Line.x2 = Convert.ToInt32(Console.ReadLine());
+             Console.Write("Enter value for y1: ");
+             Line.y1 = Convert.ToInt32(Console.ReadLine());
+             Console.Write("Enter value for y2: ");
+             Line.y2 = Convert.ToInt32(Console.ReadLine()); 
+             Line.Coordinates();
+        }
+    }
+}
