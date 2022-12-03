7af43bfab27db25f4a3153190bfd8dbc759e5a4d
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-03 19:35:52 +0530

    Using equal method to check equality

---
+++ Using equals method to check equality of two Lengths/Line.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Using_equals_method_to_check_equality_of_two_Lengths
+{
+    internal class Line
+    {
+        public static double x1, y1, x2, y2, length_of_line1, length_of_line2, x3, y3, x4, y4;
+        public static void Coordinates()
+        {
+            length_of_line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of line : " + length_of_line1);
+        }
+        public static void Coordinates1()
+        {
+            length_of_line2 = Math.Sqrt(Math.Pow(x3 - x4, 2) + Math.Pow(y3 - y4, 2));
+            Console.WriteLine("Length of line : " + length_of_line2);
+        }
+        public static void Coordinates2()
+        { 
+            Console.WriteLine(length_of_line1.Equals(length_of_line2));
+        }
+}
+}
---
+++ Using equals method to check equality of two Lengths/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Using_equals_method_to_check_equality_of_two_Lengths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value for x1: ");
+            Line.x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value for x2: ");
+            Line.x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value for y1: ");
+            Line.y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value for y2: ");
+            Line.y2 = Convert.ToInt32(Console.ReadLine());
+            Line.Coordinates();
+
+            Console.Write("\n");
+
+            Console.Write("Enter value for x3: ");
+            Line.x3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value for x4: ");
+            Line.x4 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value for y4: ");
+            Line.y3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value for y5: ");
+            Line.y4 = Convert.ToInt32(Console.ReadLine());
+            Line.Coordinates1();
+            Console.Write("\n");
+
+            Line.Coordinates2();
+
+
+        }
+    }
+}
